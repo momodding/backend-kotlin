f2463d86f800425e1656028964adfc2a6eb6b6dd
utsman <utsman@investree.id>
2020-08-03 09:19:39 +0700

    [UTSMAN] update unit test

--- src/test/kotlin/com/momodding/backend/app/service/usercredential/UserCredentialServiceTest.kt
+++ src/test/kotlin/com/momodding/backend/app/service/usercredential/UserCredentialServiceTest.kt
@@ -2,23 +2,33 @@ package com.momodding.backend.app.service.usercredential
 
 import com.momodding.backend.app.dto.request.LoginRequest
 import com.momodding.backend.app.dto.request.RegisterRequest
+import com.momodding.backend.app.dto.response.AuthResponse
 import com.momodding.backend.app.entity.UserCredential
+import com.momodding.backend.app.entity.UserToken
 import com.momodding.backend.app.repository.UserCredentialRepository
+import com.momodding.backend.app.repository.UserTokenRepository
 import com.momodding.backend.config.auth.JwtUtils
+import com.momodding.backend.config.auth.TokenPayload
 import com.momodding.backend.exception.AppException
 import com.momodding.backend.exception.DataNotFoundException
 import com.momodding.backend.exception.UnauthorizedException
+import com.momodding.backend.utils.AppProperties
+import com.momodding.backend.utils.JwtProperties
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.notNullValue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.BDDMockito.given
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Spy
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.context.annotation.Description
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import java.util.*
 
 @ExtendWith(MockitoExtension::class)
@@ -28,8 +38,15 @@ class UserCredentialServiceTest {
 	lateinit var userCredentialRepository: UserCredentialRepository
 
 	@Mock
+	lateinit var userTokenRepository: UserTokenRepository
+
+	@Mock
 	lateinit var jwtUtils: JwtUtils
 
+	@Mock
+	lateinit var appProperties: AppProperties
+
+	@Spy
 	@InjectMocks
 	lateinit var userCredentialServiceImpl: UserCredentialServiceImpl
 
@@ -38,6 +55,23 @@ class UserCredentialServiceTest {
 //		MockitoAnnotations.initMocks(this)
 //		userCredentialServiceImpl = UserCredentialServiceImpl(userCredentialRepository, jwtUtils)
 //	}
+
+	private fun <T> any(): T {
+		Mockito.any<T>()
+		return uninitialized()
+	}
+
+	private fun <T> uninitialized(): T = null as T
+
+	@BeforeEach
+	fun setUp() {
+		val jwtProperties = JwtProperties(
+				expiration = 1,
+				refresh = 1,
+				signingKey = "abcd"
+		)
+		Mockito.lenient().`when`(appProperties.jwt).thenReturn(jwtProperties)
+	}
 
 	@Test
 	@Description("login user using email and password")
@@ -55,12 +89,26 @@ class UserCredentialServiceTest {
 				username = "lol@mail.com",
 				password = "password"
 		)
+		val tokenPayload = TokenPayload(
+				ucId = user.id!!,
+				email = user.email ?: "",
+				userRole = user.role,
+				issuedAt = Date().time
+		)
+		val userToken = UserToken(
+				ucId = user.id,
+				refreshToken = BCryptPasswordEncoder().encode(tokenPayload.toString()),
+				expiredIn = Date(System.currentTimeMillis() + appProperties.jwt.refresh)
+		)
+
 		given(userCredentialRepository.findByEmail(request.username)).willReturn(Optional.of(user))
 		given(userCredentialRepository.saveAndFlush(user)).willReturn(user)
+		Mockito.`when`(userTokenRepository.saveAndFlush(Mockito.any(UserToken::class.java))).thenReturn(userToken)
+		Mockito.`when`(jwtUtils.generateToken(this.any())).thenReturn("lorem.ipsum")
 		val doLogin = userCredentialServiceImpl.doLogin(request)
 
 		assertThat(doLogin, `is`(notNullValue()))
-		assertThat((doLogin as Map<*, *>).filterKeys { it == "accessToken" }, `is`(notNullValue()))
+		assertThat((doLogin as AuthResponse).accessToken, `is`(notNullValue()))
 	}
 
 	@Test
@@ -156,13 +204,26 @@ class UserCredentialServiceTest {
 				email = "something@mail.com",
 				role = 1
 		)
+		val tokenPayload = TokenPayload(
+				ucId = 1,
+				email = user.email ?: "",
+				userRole = user.role,
+				issuedAt = Date().time
+		)
+		val userToken = UserToken(
+				ucId = 1,
+				refreshToken = BCryptPasswordEncoder().encode(tokenPayload.toString()),
+				expiredIn = Date(System.currentTimeMillis() + appProperties.jwt.refresh)
+		)
 		given(userCredentialRepository.findByEmail(request.email)).willReturn(null)
 		given(userCredentialRepository.saveAndFlush(user)).willReturn(user)
+		Mockito.`when`(userTokenRepository.saveAndFlush(Mockito.any(UserToken::class.java))).thenReturn(userToken)
+		Mockito.`when`(jwtUtils.generateToken(this.any())).thenReturn("lorem.ipsum")
 
 		val doRegister = userCredentialServiceImpl.doRegister(request)
 
 		assertThat(doRegister, `is`(notNullValue()))
-		assertThat((doRegister as Map<*, *>).filterKeys { it == "accessToken" }, `is`(notNullValue()))
+		assertThat((doRegister as AuthResponse).accessToken, `is`(notNullValue()))
 	}
 
 	@Test
