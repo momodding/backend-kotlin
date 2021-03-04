7d727921aca4b72d0324e4233acffc6913dccc5b
utsman <utsman@investree.id>
2021-03-04 12:46:24 +0700

    [UTSMAN] move env variable

--- src/main/kotlin/com/momodding/backend/BackendApplication.kt
+++ src/main/kotlin/com/momodding/backend/BackendApplication.kt
@@ -15,18 +15,5 @@ import org.springframework.core.env.StandardEnvironment
 class BackendApplication
 
 fun main(args: Array<String>) {
-	try {
-		val dotenv = dotenv().entries().associateByTo(hashMapOf(), { it.key }, { it.value }).toMap()
-		SpringApplicationBuilder(BackendApplication::class.java)
-				.environment(object : StandardEnvironment() {
-					override fun customizePropertySources(propertySources: MutablePropertySources) {
-						super.customizePropertySources(propertySources)
-						propertySources.addLast(MapPropertySource("dotenvProps", dotenv))
-						System.out.println(propertySources.get("dotenvProps"))
-					}
-				})
-				.run(*args)
-	} catch (ex: Exception) {
-		runApplication<BackendApplication>(*args)
-	}
+	runApplication<BackendApplication>(*args)
 }
