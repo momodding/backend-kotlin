96db71e00bf517719642ca84501b17aafb2f92e6
utsman <utsman@investree.id>
2021-05-09 18:12:02 +0700

    [UTSMAN] return when unauthorized

--- src/main/kotlin/com/momodding/backend/config/websecurity/WebSecurityConfig.kt
+++ src/main/kotlin/com/momodding/backend/config/websecurity/WebSecurityConfig.kt
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
@@ -48,7 +49,7 @@ class WebSecurityConfig @Autowired constructor(
 				.antMatchers(*whitelistEndpoint).permitAll()
 				.anyRequest().authenticated()
 				.and()
-				.addFilter(jwtAuthFilter)
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
 		}
 	}
 
